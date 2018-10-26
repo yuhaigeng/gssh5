895c450a9ce040dc067940584b2f646572156bbd
zhaoyuhua <zhaoyuhua_2014@163.com>
2018-10-26 11:21:42 +0800

    框架搭建

---
+++ src/page/more/moreGoods.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="moreGoods">
+        <app-header></app-header>
+        <h1>more</h1>
+        <app-footer></app-footer>
+   </div>
+</template>
+
+<script>
+import appHeader from "../../components/public/header.vue";
+import appFooter from "../../components/public/footer.vue";
+export default {
+   name: 'more',
+   data() {
+       return {}
+   },
+  components: {
+    appHeader,
+    appFooter
+  }
+}
+</script>
+
+<style scoped >
+    
+
+</style>
---
+++ src/page/my/my.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="my">
+        <app-header></app-header>
+        <h1>my</h1>
+        <app-footer></app-footer>
+   </div>
+</template>
+
+<script>
+import appHeader from "../../components/public/header.vue";
+import appFooter from "../../components/public/footer.vue";
+export default {
+   name: 'my',
+   data() {
+       return {}
+   },
+  components: {
+    appHeader,
+    appFooter
+  }
+}
+</script>
+
+<style scoped >
+    
+
+</style>
