f3191db6d60a3510f1377d58235786e7622d450d
zxf <1837244680@qq.com>
2018-12-06 14:01:35 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 
 
 import axios from 'axios' //引入axios
@@ -40,6 +41,7 @@ import Vuelazyload from "vue-lazyload"
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
